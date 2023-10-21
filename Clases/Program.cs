852fc235cbb3e1fc850197eece93afc7276701cb
gurdian0614 <ivan.gurdian@outlook.com>
2023-10-20 18:47:57 -0600

    Add project files.

---
+++ Clases/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+public class Ave
+{
+    public string nombre { get; set; }
+    public string color { get; set; }
+    public string tamano { get; set; }
+    public string familia { get; set; }
+    public string sexoBiologico { get; set; }
+    public bool esVoladora { get; set; }
+
+    /**
+     * Constructor
+     */
+    public Ave() {
+        esVoladora = true;
+        color = "rojo";
+    }
+
+}
